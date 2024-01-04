51b68f2dc6d72ca9454a9d38d4051fa6e85a1940
Dragos <mailsafe923@gmail.com>
2024-01-04 16:11:53 +0200

    Added the code for parsing and downloading the CSV file in multiple, parameter-delimited CSV files.

--- parse_file.py
+++ parse_file.py
@@ -1,22 +1,33 @@
 import pandas as pd
 
+#TODO To make a function for both reading and saving as CSV or XLSX
 df = pd.read_csv('D:\\Downloads\\Date_CSV.csv')
-col_length = df.shape[1]
 col_names = df.columns.tolist()
+csv_names = ['Temp', 'Umiditate', 'Prezenta', 'Viteza', 'Pozitie']
+temp_file = []
+umid_file = []
+prezenta_file = []
+viteza_file = []
+pozitie_file = []
+file_list = {'Temp': temp_file, 'Umiditate': umid_file, 'Prezenta': prezenta_file, 'Pozitie': pozitie_file,
+             'Viteza': viteza_file}
+
 for index, name in enumerate(col_names):
+    file = pd.read_csv('D:\\Downloads\\Date_CSV.csv', header=0, usecols=[index])
     if 'Temp' in name:
-        col_names[index] = 'Temp'
+        temp_file.append(file)
     elif 'Umiditate' in name:
-        col_names[index] = 'Umiditate'
+        umid_file.append(file)
     elif 'Prezenta' in name:
-        col_names[index] = 'Prezenta'
+        prezenta_file.append(file)
+    elif 'Pozitie' in name:
+        pozitie_file.append(file)
     elif 'Viteza' in name:
-        col_names[index] = 'Viteza'
-print(col_names)
-for column in range(1, col_length):
-    df = pd.read_csv('D:\\Downloads\\Date_CSV.csv', header=0,
-                     usecols=[column])
-#TODO To eliminate the repeating names of the columns in the list and make a file with each parameter
+        viteza_file.append(file)
 
 
-
+for file in file_list:
+    if file_list.get(file):
+        print(file_list.get(file))
+        csv_file = pd.DataFrame(file_list.get(file))
+        csv_file.to_csv(f'D:\\Downloads\\{file}.csv')
