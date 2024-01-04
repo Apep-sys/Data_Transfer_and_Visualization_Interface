40a6c97d76505d7e79a5c65d694918694483314e
Apep <mailsafe923@gmail.com>
2024-01-04 17:36:40 +0200

    Created functions for processing the csv file and processing the xlsx file. Both will create separate files, depending on the parameter they contain.

--- parse_file.py
+++ parse_file.py
@@ -1,33 +1,66 @@
 import pandas as pd
 
-#TODO To make a function for both reading and saving as CSV or XLSX
-df = pd.read_csv('D:\\Downloads\\Date_CSV.csv')
-col_names = df.columns.tolist()
-csv_names = ['Temp', 'Umiditate', 'Prezenta', 'Viteza', 'Pozitie']
-temp_file = []
-umid_file = []
-prezenta_file = []
-viteza_file = []
-pozitie_file = []
-file_list = {'Temp': temp_file, 'Umiditate': umid_file, 'Prezenta': prezenta_file, 'Pozitie': pozitie_file,
-             'Viteza': viteza_file}
 
-for index, name in enumerate(col_names):
-    file = pd.read_csv('D:\\Downloads\\Date_CSV.csv', header=0, usecols=[index])
-    if 'Temp' in name:
-        temp_file.append(file)
-    elif 'Umiditate' in name:
-        umid_file.append(file)
-    elif 'Prezenta' in name:
-        prezenta_file.append(file)
-    elif 'Pozitie' in name:
-        pozitie_file.append(file)
-    elif 'Viteza' in name:
-        viteza_file.append(file)
+def press_csv():
+    pass
 
 
-for file in file_list:
-    if file_list.get(file):
-        print(file_list.get(file))
-        csv_file = pd.DataFrame(file_list.get(file))
-        csv_file.to_csv(f'D:\\Downloads\\{file}.csv')
+def process_csv():
+    df = pd.read_csv('D:\\Downloads\\Date_CSV.csv')
+    col_names = df.columns.tolist()
+    temp_file = []
+    umid_file = []
+    prezenta_file = []
+    viteza_file = []
+    pozitie_file = []
+    file_list = {'Temp': temp_file, 'Umiditate': umid_file, 'Prezenta': prezenta_file, 'Pozitie': pozitie_file,
+                 'Viteza': viteza_file}
+
+    for name in col_names:
+        file = df[name].tolist()
+        if 'Temp' in name:
+            temp_file.extend(file)
+        elif 'Umiditate' in name:
+            umid_file.extend(file)
+        elif 'Prezenta' in name:
+            prezenta_file.extend(file)
+        elif 'Pozitie' in name:
+            pozitie_file.extend(file)
+        elif 'Viteza' in name:
+            viteza_file.extend(file)
+
+    for file in file_list:
+        if file_list.get(file):
+            csv_file = pd.DataFrame({file: file_list.get(file)})
+            csv_file.to_csv(f'D:\\Downloads\\{file}.csv', index_label='Index')
+
+
+def process_xlsx():
+    df = pd.read_excel('D:\\Downloads\\Date_CSV.xlsx', sheet_name='Sheet 1', index_col=0)
+    col_names = df.columns.tolist()
+    temp_file = []
+    umid_file = []
+    prezenta_file = []
+    viteza_file = []
+    pozitie_file = []
+    file_list = {'Temp': temp_file, 'Umiditate': umid_file, 'Prezenta': prezenta_file, 'Pozitie': pozitie_file,
+                 'Viteza': viteza_file}
+
+    for name in col_names:
+        column_data = df[name].tolist()
+        if 'Temp' in name:
+            temp_file.extend(column_data)
+        elif 'Umiditate' in name:
+            umid_file.extend(column_data)
+        elif 'Prezenta' in name:
+            prezenta_file.extend(column_data)
+        elif 'Pozitie' in name:
+            pozitie_file.extend(column_data)
+        elif 'Viteza' in name:
+            viteza_file.extend(column_data)
+
+    for file in file_list:
+        if file_list.get(file):
+            df_file = pd.DataFrame({file: file_list.get(file)})
+            df_file.to_excel(f'D:\\Downloads\\{file}.xlsx', sheet_name='Sheet 1', index_label='Index')
+
